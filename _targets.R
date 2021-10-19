62f9c85df03dab3d05bc7829299a0458b27d128a
Adam M. Wilson <adamw@buffalo.edu>
2021-10-19 14:38:08 -0400

    update files

--- _targets.R
+++ _targets.R
@@ -37,8 +37,8 @@ list(
              cmdstan_model('firemodel_predict.stan',
                            compile = TRUE)),
 
-tar_target(model_fit, fit_model(model, stan_data,  "model.rds"),format="file"),
-tar_target(model_output, readRDS(model_fit)),
+tar_target(model_output, fit_model(model, stan_data,  "model.rds"),format="file"),
+#tar_target(model_output, readRDS(model_fit)),
 
 tar_target(posterior_summary,
            summarize_posteriors(model_output,data)),
