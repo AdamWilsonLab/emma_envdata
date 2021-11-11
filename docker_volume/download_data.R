397dee9d135c6522b3a482ffb12758ac04459a6c
Brian <bmaitner@gmail.com>
2021-11-11 14:46:04 -0500

    scripts for downloading alos data

---
+++ docker_volume/download_data.R
@@ -0,0 +1,40 @@
+#Code for downloading raw data used by the EMMA project
+  #This code was written to work with a particular docker instance, to set it up (modifying your directory accordingly):
+  #docker run -d -e DISABLE_AUTH=true -p 8787:8787 -v C:/Users/"Brian Maitner"/Desktop/current_projects/emma_targets/docker_volume:/home/rstudio/docker_volume adamwilsonlab/emma_docker:latest
+
+
+#Install packages (note: better to make these part of the docker)
+  install.packages(c("gdalUtils","qpdf","RefManageR","svMisc"))
+  install.packages("bibtex")
+  install.packages("https://gitlab.rrz.uni-hamburg.de/helgejentsch/climdatdownloadr/-/archive/master/climdatdownloadr-master.tar.gz", repos = NULL, type = "source")
+  install.packages("raster")
+
+library(rgee)
+library(ClimDatDownloadR)
+
+#To run once
+  #Again, would be better to make these part of the docker
+  
+  ee_install(confirm = FALSE) #note: would probably be faster to update the image to contain python, numpy, etc
+  .rs.restartR() #this is needed to restart R after the installations that come in the predicting line of code
+  
+  ee_install_upgrade()
+
+
+
+#Elevation (NASADEM)
+  #source("docker_volume/R/elevation.R") #this will take a while
+  sadem <- raster("docker_volume/raw_data/NASADEM.tif")
+
+#Climate (CHELSA)
+  #source("docker_volume/R/climate.R")
+  
+#Precipitation
+  #source("docker_volume/R/precipitation_chelsa.R")
+  
+  
+#ALOS variables from GEE - TPI, insolation, landforms, mTPI, diversity, CHILI  
+  
+  
+  
+  
