7e3bbaeee01d8730986522d1db8188f54a28a6a3
Brian <bmaitner@gmail.com>
2021-11-17 12:30:02 -0500

    issues in the volume setup I tried (causing issues in python installation), so I modified it slightly

--- docker_volume/1_download_data.R
+++ docker_volume/1_download_data.R
@@ -4,23 +4,30 @@
 
 
   #This code was written to work with a particular docker instance, to set it up (modifying your directory accordingly):
-  ##docker run -d -e DISABLE_AUTH=true -p 8787:8787 -v C:/Users/"Brian Maitner"/Desktop/current_projects/emma_targets/docker_volume:/home/rstudio/docker_volume adamwilsonlab/emma_docker:latest
-  #docker run -d -e DISABLE_AUTH=true -p 8787:8787 -v C:/Users/"Brian Maitner"/Desktop/current_projects/emma_targets:/home/rstudio adamwilsonlab/emma_docker:latest
+  #docker run -d -e DISABLE_AUTH=true -p 8787:8787 -v C:/Users/"Brian Maitner"/Desktop/current_projects/emma_targets:/home/rstudio/emma_targets adamwilsonlab/emma_docker:latest
+
+  #the code below would be more useful (wouldn't require the change in wd), but causes problem for installation
+  ##docker run -d -e DISABLE_AUTH=true -p 8787:8787 -v C:/Users/"Brian Maitner"/Desktop/current_projects/emma_targets:/home/rstudio adamwilsonlab/emma_docker:latest
+
+# Code to ensure the directory in docer matches the directory in the github project in rstudio
+  if(getwd()=="/home/rstudio"){setwd("/home/rstudio/emma_targets/")}
+
 
 #Install packages (note: better to make these part of the docker image)
   install.packages(c("gdalUtils","qpdf","RefManageR","svMisc","bibtex","raster","rdryad","geojsonio"))
   install.packages("https://gitlab.rrz.uni-hamburg.de/helgejentsch/climdatdownloadr/-/archive/master/climdatdownloadr-master.tar.gz", repos = NULL, type = "source")
 
 library(rgee)
-library(ClimDatDownloadR)
 
 #To run once
   #Again, would be better to make these part of the docker
+  reticulate::install_miniconda() #note that there is a misnamed component file in ncurses
+  reticulate::install_python()
 
+#Old (works in docker_image as a volume)
   ee_install(confirm = FALSE) #note: would probably be faster to update the image to contain python, numpy, etc
-  .rs.restartR() #this is needed to restart R after the installations that come in the predicting line of code
+  .rs.restartR() #this is needed to restart R after the installations that comes in the preceding line of code
   ee_clean_pyenv()
-
 
   ee_install_upgrade()
   ee_clean_pyenv()
