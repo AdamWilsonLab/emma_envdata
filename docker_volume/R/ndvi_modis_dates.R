5f1d22bb75f749d629c1242d0e559d589de0cc70
Brian <bmaitner@gmail.com>
2021-11-22 10:57:44 -0500

    updated ndvi dates code to do all calculations on the google end of things using the $map() function.

--- docker_volume/R/ndvi_modis_dates.R
+++ docker_volume/R/ndvi_modis_dates.R
@@ -1,7 +1,7 @@
 # Get MODIS NDVI dates
 
 
-#' @author Brian Maitner, but built from code by Qinwen and Adam
+#' @author Brian Maitner, with tips from csaybar
 
 #Load packages
 library(rgee)
@@ -34,119 +34,36 @@ modis_ndvi <- ee$ImageCollection("MODIS/006/MOD13A2")
   #ymd(year) + DayOfYear - 1 (need to subtract 1 since values start with 1, not zero)
 
 
-#MODIS makes it simple to filter out poor quality pixels thanks to a quality control bits band (DetailedQA).
-#The following function helps us to distinct between good data (bit == …00) and marginal data (bit != …00).
-
-  getQABits <- function(image, qa) {
-    # Convert binary (character) to decimal (little endian)
-    qa <- sum(2^(which(rev(unlist(strsplit(as.character(qa), "")) == 1))-1))
-    # Return a mask band image, giving the qa value.
-    image$bitwiseAnd(qa)$lt(1)
-  }
-
-  mod13A2_clean <- function(img) {
-
-    # Extract the NDVI band
-    ndvi_values <- img$select("NDVI")
-
-    # Extract the quality band
-    ndvi_qa <- img$select("SummaryQA")
-
-    # Select pixels to mask
-    quality_mask <- getQABits(ndvi_qa, "11")
-
-    # Mask pixels with value zero.
-    ndvi_values$updateMask(quality_mask)
-  }
-
-
-#Clean the dataset
-
-  ndvi_clean <- modis_ndvi$map(mod13A2_clean)
-
 
 ########################################
 
 
-#So, we want a function that takes in an image and returns the data as integer format
+  get_integer_date <-function(img) {
 
-    get_integer_composite_date <-function(img) {
-
-    # extract the DayOfYear band
+    # 1. Extract the DayOfYear band
       day_values <- img$select("DayOfYear")
-      # Get the first day of the year and convert to an integer.
 
-      #the next three lines of code need to be modified to work in gee
-        start_date <- rgee::ee_get_date_img(img)$time_start #can't use R code with map
-        #start_date_ms <- img$getInfo()$properties$`system:index` #get the start date
-        #start_date <- ee$Image$getInfo(img)$properties$`system:index`
+    # 2. Get the first day of the year and the UNIX base date.
+      first_doy <- ee$Date(day_values$get("system:time_start"))$format("Y")
+      base_date <- ee$Date("1970-01-01")
 
-        focal_origin <- lubridate::floor_date(x = start_date,unit = "years") #this gets the first date of the year.  all modis dates are relative to this
-        #focal_origin <- paste(strsplit(x = start_date_ms,split = "_")[[1]][1],"-01-01",sep = "")
 
-        focal_origin <- as.numeric(as.Date(focal_origin))-1 #1 is subtracted so that a value of 1 on the raster corresponds to a Jan 1, not Jan 2
+    # 3. Get the day diff between year start_date and UNIX date
+      daydiff <- ee$Date(first_doy)$difference(base_date,"day")
 
-    #Mask to only values greater than zero.
-      #Personal note: whoever coined this term is a sadist.
-      #This is the exact opposite of how a real mask works and always confuses me.
-
+    # 4. Mask to only values greater than zero.
       mask <- day_values$gt(0)
-      day_values <- day_values$updateMask(mask) #masked values read as 0
+      day_values <- day_values$updateMask(mask)
 
     # #Now, I just need to add the origin to the map
-      day_values$add(focal_origin)
-
-    }
+      day_values$add(daydiff)
+  }
 
 ############################################
 
-#Since the above function doesn't work with $map() (yet...), I'll wrap it in a for loop and download the layers one by one instead
-  #Note that this is temporary, and I intend to modify this to play with the burn date code as well
+ndvi_integer_dates <- modis_ndvi$map(get_integer_date)
 
-#get a list of all dates or ids
-
-modis_dates <- ee_get_date_ic(modis_ndvi)
-
-#get the transformed date layer for each
-
-  for( i in 1:nrow(modis_dates)){
-
-
-    date_i <- modis_dates$time_start[i]
-    date_i <- gsub(pattern = " GMT",replacement = "",x = date_i)
-    img_i <- modis_ndvi$filterDate(date_i)
-
-    #check that theres only one layer
-    if(length(img_i$getInfo()$features)!=1){stop("Too many layers")}
-
-    #turn from collection to image
-    img_i <- img_i$first()
-
-    modis_integer_date <- get_integer_composite_date(img = img_i)
-
-    #Code to download (code to read in burn date rasters will go here)
-    ee_as_raster(image = modis_integer_date,
-                 region = sabb,
-                 dsn = paste("docker_volume/raw_data/modis_ndvi_dates/",
-                             gsub(pattern = "-",replacement = "_",x = date_i),
-                             sep = ""))
-
-
-
-
-  }
-
-
-################################################
-
-  #scratch code
-modis_integer_date <- get_integer_composite_date(img = img)
-#modis_integer_date_collection <- modis_ndvi$map(get_integer_composite_date) #doesn't work yet
-Map$addLayer(modis_integer_date,
-             visParams =list(
-               min = 11000,
-               max = 11050,
-               palette = c('00FFFF','FF00FF')
-               ))
-
+ee_imagecollection_to_local(ic = ndvi_integer_dates,
+                            region = sabb,
+                            dsn = "docker_volume/raw_data/modis_ndvi_dates/")
 
