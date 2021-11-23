a8682d6ab4d20f729b4c2d6756c446e435dad3f4
Brian <bmaitner@gmail.com>
2021-11-23 17:36:24 -0500

    progress on aligning modis NDVI dates with MODIS fire dates. Still need to verify the "most recent fire date" rasters are correct and then it should be just a matter of subtracting the corresponding "most recent fire" rasters from the "modis date" rasters to get the time since fire

--- docker_volume/R/make_burn_date_raster.R
+++ docker_volume/R/make_burn_date_raster.R
@@ -24,30 +24,24 @@ adjust_burndate_to_modis <- function(burn_collection,modis_collection){
 
   #1) convert dates to integer date since unix era
 
-        get_integer_date_fire <-function(img) {
 
-          # 1. Extract the DayOfYear band
-          day_values <- img$select("BurnDate")
+  get_integer_date_fire <-function(img) {
 
-          # 2. Get the first day of the year and the UNIX base date.
-          first_doy <- ee$Date(day_values$get("system:time_start"))$format("Y")
-          base_date <- ee$Date("1970-01-01")
+    # 1. Extract the DayOfYear band
+    day_values <- img$select("BurnDate") #still works as of this line
 
+    # 2. Get the first day of the year and the UNIX base date.
+    first_doy <- ee$Date(day_values$get("system:time_start"))$format("Y")
+    base_date <- ee$Date("1970-01-01")
+    # 3. Get the day diff between year start_date and UNIX date
+    daydiff <- ee$Date(first_doy)$difference(base_date,"day")
 
-          # 3. Get the day diff between year start_date and UNIX date
-          daydiff <- ee$Date(first_doy)$difference(base_date,"day")
+    day_values <- day_values$add(daydiff$int())$copyProperties(img) #this step makes "day_values" and element, rather than image
 
-          # 4. Mask to only values greater than zero.
-          mask <- day_values$gt(0)
-          day_values <- day_values$updateMask(mask)
+    day_values <- ee$Image(day_values$copyProperties(img))#this turns things back into an image
+    day_values$toInt()    #this converts things to integer
 
-          # Now, I just need to add the origin to the map
-          #Note the use of "copyProperties", which copies over the start and end times
-          day_values$
-            add(daydiff)$
-            copyProperties(img, c('system:id', 'system:time_start','system:time_end'))
-
-        }
+  }
 
 
     fire_integer <- fire_clean$map(get_integer_date_fire)
@@ -60,28 +54,47 @@ adjust_burndate_to_modis <- function(burn_collection,modis_collection){
 
         #A) Get the date of the image (so we can pull older stuff)
 
-
-
             focal_data <- img$select("BurnDate")
             start_date <- ee$Date(focal_data$get("system:time_start"))
             end_date <- ee$Date(focal_data$get("system:time_end"))
 
         #B) Select the older rasters
             focal_dataset <- fire_integer$filterDate(start = "1900-01-01",
-                                                     opt_end = start_date)
+                                                     opt_end = end_date)
 
-
-            focal_dataset$getInfo()
-            focal_dataset$aggregate_max()
-
+            focal_dataset$max()$copyProperties(img, c('system:id', 'system:time_start','system:time_end'))
 
     }
 
+
+    fire_continuous_integer <- fire_integer$map(last_fire_date)
 
   #3) Convert date of last burn to time since last burn
       #corresponding modis NDVI date - burn date for corresponding image
 
 
+
+
 }#end fx
 
 #################
+
+#Works
+
+##########################
+#Debugging max()
+Map$addLayer(eeObject = fire_clean$max(),
+             fireviz <- list(
+               min = fire_clean$getInfo()$properties$visualization_0_min,
+               max = fire_clean$getInfo()$properties$visualization_0_max,
+               palette = c('00FFFF','FF00FF')
+             ))#works just fine works
+
+Map$addLayer(fire_integer$max(),
+             visParams = list(
+               min = 11262,
+               max = 18932,
+               palette = c('00FFFF','FF00FF')
+             ))
+
+#Next step: double check that the last fire date function is performing appropriately
