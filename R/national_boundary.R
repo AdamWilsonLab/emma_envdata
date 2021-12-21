f0b0edd0352b4edc28d9cd17af63c9a024d71e6a
Adam M. Wilson (horae) <adamw@buffalo.edu>
2021-12-21 09:57:49 -0500

    push vegetation data to LFS

--- R/national_boundary.R
+++ R/national_boundary.R
@@ -1,25 +1,22 @@
-#ALOS
+# National Boundary
 
 #' @author Adam M. Wilson
-
-#' Download national boundary file from the UN
-
-#' @param url is the text string used by gee to refer to an image, e.g. "CSP/ERGo/1_0/Global/ALOS_mTPI"
-#' @param sabb Bounding Box to constrain area downloaded
-#' @note This code is only deisnged to work with a handful of images by CSP/ERGo
+#' @description Download national boundary file from the UN
 #' @source  https://data.humdata.org/dataset/south-africa-admin-level-1-boundaries
 
-national_boundary <- function(){
+national_boundary <- function(file="data/south_africa.gpkg"){
+  destfile="data/zaf_adm_sadb_ocha_20201109_SHP/zaf_admbnda_adm0_sadb_ocha_20201109.shp"
 
-  url="https://data.humdata.org/dataset/061d4492-56e8-458c-a3fb-e7950991adf0/resource/f5b08257-8d03-48dc-92c8-aaa4fb7285f0/download/zaf_adm_sadb_ocha_20201109_shp.zip"
-
-  tmpfile=tempfile()
-  download.file(url,destfile = tmpfile)
-  unzip(tmpfile,exdir="data")
-
-  za=st_read("data/zaf_adm_sadb_ocha_20201109_SHP/zaf_admbnda_adm0_sadb_ocha_20201109.shp")
-
-  return(za)
+  if(!file.exists(destfile)){
+    url="https://data.humdata.org/dataset/061d4492-56e8-458c-a3fb-e7950991adf0/resource/f5b08257-8d03-48dc-92c8-aaa4fb7285f0/download/zaf_adm_sadb_ocha_20201109_shp.zip"
+    tmpfile1=tempfile()
+    tmpdir1=tempdir()
+    download.file(url,destfile = tmpfile1)
+    unzip(tmpfile,exdir=tmpdir1)
+  }
+    country=st_read(file.path(tmpdir1,"zaf_adm_sadb_ocha_20201109_SHP/zaf_admbnda_adm0_sadb_ocha_20201109.shp"))
+    st_write(country,dsn=file,append=F)
+  return(file)
 }
 # end function
 
