f967fe985a516dc587b1910cf549fab0d30c341b
Adam M. Wilson (horae) <adamw@buffalo.edu>
2021-12-22 18:23:21 -0500

    remove renv

--- R/model_data.R
+++ R/model_data.R
@@ -1,10 +1,14 @@
 # generate the model data object
 
 
-get_model_data <- function(remnant_distance, file="data/model_data.csv"){
+get_model_data <- function(remnant_distance, alos, file="data/model_data.csv"){
 
   tar_load(remnant_distance)
 
+
+
+
+  # build the data.frame!
   env=bind_cols(
     remnant_distance=as.data.frame(stars::read_stars(remnant_distance))
   ) %>%
