57c53871c0811b1b58315654ab4994f3739d6230
Brian <bmaitner@gmail.com>
2022-01-04 15:18:12 -0500

    minor updates to alos code.  removed old code from "docker_image" folder

--- R/get_alos.R
+++ R/get_alos.R
@@ -5,28 +5,32 @@
 #make a function to reduce code duplication
 
 #' @param image_text is the text string used by gee to refer to an image, e.g. "CSP/ERGo/1_0/Global/ALOS_mTPI"
-#' @param sabb Bounding Box to constrain area downloaded
-#' @note This code is only deisnged to work with a handful of images by CSP/ERGo
-get_alos_data <- function(image_text,sabb){
+#' @param directory Bounding Box to constrain area downloaded
+#' @note This code is only designed to work with a handful of images by CSP/ERGo
+get_alos_data <- function(image_text,dir){
 
+  #Load the image
 
-  focal_image <- ee$Image(image_text)
+    focal_image <- ee$Image(image_text)
 
+    focal_name <- focal_image$getInfo()$properties$visualization_0_name
 
-  focal_name <- focal_image$getInfo()$properties$visualization_0_name
-  focal_name <- tolower(focal_name)
-  focal_name <-gsub(pattern = " ", replacement = "_", x = focal_name)
+    focal_name <- tolower(focal_name)
 
+    focal_name <-gsub(pattern = " ", replacement = "_", x = focal_name)
+
+  #get the domain
+    domain <- get_domain()
 
   #get CRS
-  crs <- focal_image$getInfo()$bands[[1]]$crs
+    crs <- focal_image$getInfo()$bands[[1]]$crs
 
   #Download the raster
-  ee_as_raster(image = focal_image,
-               region = sabb,
-               #scale = 100, #used to adjust the scale. commenting out uses the default
-               dsn = paste("data/raw_data/alos/",focal_name,sep = ""),
-               maxPixels = 10000000000)
+    ee_as_raster(image = focal_image,
+                 region = domain,
+                 #scale = 100, #used to adjust the scale. commenting out uses the default
+                 dsn = paste(dir,focal_name,sep = ""),
+                 maxPixels = 10000000000)
 
 
 }# end function
@@ -38,47 +42,40 @@ get_alos <- function(directory = "data/raw_data/alos/"){
 
   #make a directory if one doesn't exist yet
 
-  if(!dir.exists(directory)){
-    dir.create(directory)
-  }
+    if(!dir.exists(directory)){
+      dir.create(directory)
+    }
 
-  #Make a bounding box of the extent we want
-
-  ext <- readRDS(file = "data/other_data/domain_extent.RDS")
-
-  sabb <- ee$Geometry$Rectangle(
-    coords = c(ext@xmin,ext@ymin,ext@xmax,ext@ymax),
-    proj = "EPSG:4326",
-    geodesic = FALSE
-  )
-
-  rm(ext)
 
   # Get files that have been downloaded
-  alos_files <- list.files(directory,pattern = ".tif$")
+    alos_files <- list.files(directory,pattern = ".tif$")
 
   #Download files that have not previously been downloaded
 
   # mTPI
-  if(!length(grep(pattern = "mtpi",x = alos_files)) > 0){
-    get_alos_data(image_text = "CSP/ERGo/1_0/Global/ALOS_mTPI")
-  }
+    if(!length(grep(pattern = "mtpi",x = alos_files)) > 0){
+      get_alos_data(image_text = "CSP/ERGo/1_0/Global/ALOS_mTPI",
+                    dir = directory)
+    }
 
   # CHILI
-  if(!length(grep(pattern = "chili",x = alos_files)) > 0){
-    get_alos_data(image_text = "CSP/ERGo/1_0/Global/ALOS_CHILI")
-  }
+    if(!length(grep(pattern = "chili",x = alos_files)) > 0){
+      get_alos_data(image_text = "CSP/ERGo/1_0/Global/ALOS_CHILI",
+                    dir = directory)
+    }
 
 
   # landforms
-  if(!length(grep(pattern = "landforms",x = alos_files)) > 0){
-    get_alos_data(image_text = 'CSP/ERGo/1_0/Global/ALOS_landforms')
-  }
+    if(!length(grep(pattern = "landforms",x = alos_files)) > 0){
+      get_alos_data(image_text = 'CSP/ERGo/1_0/Global/ALOS_landforms',
+                    dir = directory)
+    }
 
   # topo diversity
-  if(!length(grep(pattern = "topographic",x = alos_files)) > 0){
-    get_alos_data(image_text = 'CSP/ERGo/1_0/Global/ALOS_topoDiversity')
-  }
+    if(!length(grep(pattern = "topographic",x = alos_files)) > 0){
+      get_alos_data(image_text = 'CSP/ERGo/1_0/Global/ALOS_topoDiversity',
+                    dir = directory)
+    }
 
 
   message("Finished downloading ALOS layers")
--- docker_volume/1_download_data.R
+++ docker_volume/1_download_data.R
@@ -10,7 +10,7 @@
   ##docker run -d -e DISABLE_AUTH=true -p 8787:8787 -v C:/Users/"Brian Maitner"/Desktop/current_projects/emma_targets:/home/rstudio adamwilsonlab/emma_docker:latest
 
 # Code to ensure the directory in docer matches the directory in the github project in rstudio
-  if(getwd()=="/home/rstudio"){setwd("/home/rstudio/emma_targets/")}
+  if(getwd() == "/home/rstudio"){setwd("/home/rstudio/emma_targets/")}
 
 
 #Install packages not in docker image
@@ -34,9 +34,11 @@ library(rgee)
     if(!dir.exists("data/raw_data/")){dir.create("data/raw_data/")}
     if(!dir.exists("data/processed_data/")){dir.create("data/processed_data/")}
 
+# Infrequent (~ annual?) downloads
+
   #Elevation (NASADEM)
-    source("R/get_elevation_nasadem.R")
-    get_elevation_nasadem()
+  source("R/get_elevation_nasadem.R")
+  get_elevation_nasadem()
 
   #Climate (CHELSA)
     source("R/get_climate_chelsa.R")
@@ -46,18 +48,6 @@ library(rgee)
     source("R/get_precipitation_chelsa.R")
     get_precipitation_chelsa()
 
-  #NDVI
-    source("R/get_ndvi_modis.R")
-    get_ndvi()
-
-  #KNDVI
-    source("R/get_kndvi_modis.R")
-    get_kndvi()
-
-  #MODIS dates
-    source("R/get_ndvi_dates_modis.R")
-    get_ndvi_dates_modis()
-
   #Clouds
     source("R/get_clouds_wilson.R")
     get_clouds_wilson()
@@ -65,6 +55,20 @@ library(rgee)
   #Landcover from ZA
     source("R/get_landcover_za.R")
     get_landcover_za()
+
+  #ALOS variables from GEE - TPI, insolation, landforms, mTPI, diversity, CHILI
+    source("R/get_alos.R")
+    get_alos()
+
+# Frequent (~ weekly - monthly)
+
+  #NDVI
+    source("R/get_ndvi_modis.R")
+    get_ndvi()
+
+  #KNDVI
+    source("R/get_kndvi_modis.R")
+    get_kndvi()
 
   #Fire from MODIS
     source("R/get_fire_modis.R")
@@ -74,8 +78,6 @@ library(rgee)
     source("R/get_ndvi_dates_modis.R")
     get_ndvi_dates_modis()
 
-  #ALOS variables from GEE - TPI, insolation, landforms, mTPI, diversity, CHILI
-   #need to update the alos code to use stored stuff
 
 #Process data (order matters)
 
@@ -98,7 +100,7 @@ library(rgee)
 
       # Note: should update code that processes unix date into most recent burn to optionally take in the SA burn polygons
 
-      # Note: are the above intermediates useful or should things be rolled together?  It would probably be slower but require less space
+      # Note: are the "process_data" intermediates useful or should things be rolled together?  It would probably be slower but require less space
 
       # Note: if we need to save space and don't need the fire DOY layers or other intermediates, we can clear out all but the last layer (since the ee downloads are based on the most recent layer)
 
