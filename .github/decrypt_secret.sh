8f4a74e20b157e08feb6f56b14548631b0e60de2
Brian Maitner <bmaitner@gmail.com>
2022-01-31 16:45:31 -0500

    trying to get permissions working

--- .github/decrypt_secret.sh
+++ .github/decrypt_secret.sh
@@ -4,6 +4,9 @@
 
 mkdir -p $HOME/.config/earthengine/ndef/
 
+mkdir -p /home/rstudio/.config/earthengine/ndef/
+
+
 # --batch to prevent interactive command
 # --yes to assume "yes" for questions
 
@@ -12,3 +15,9 @@ gpg --quiet --batch --yes --decrypt --passphrase="$RGEE_SECRET" \
 
 gpg --quiet --batch --yes --decrypt --passphrase="$RGEE_SECRET" \
 --output $HOME/.config/earthengine/ndef/credentials ./scratch_code/credentials.gpg
+
+gpg --quiet --batch --yes --decrypt --passphrase="$RGEE_SECRET" \
+--output /home/rstudio/.config/earthengine/ndef/20061abcbc1c6ecf51bd9cf7e37350f6_bmaitner@gmail.com ./scratch_code/20061abcbc1c6ecf51bd9cf7e37350f6_bmaitner@gmail.com.gpg
+
+gpg --quiet --batch --yes --decrypt --passphrase="$RGEE_SECRET" \
+--output /home/rstudio/.config/earthengine/ndef/credentials ./scratch_code/credentials.gpg
