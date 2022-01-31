d26650e24874cb66c701976cd58a261e88eaecd6
Brian Maitner <bmaitner@gmail.com>
2022-01-31 15:31:41 -0500

    trying to get permissions working

--- .github/decrypt_secret.sh
+++ .github/decrypt_secret.sh
@@ -8,7 +8,7 @@ mkdir -p $HOME/.config/earthengine/ndef/
 # --yes to assume "yes" for questions
 
 gpg --quiet --batch --yes --decrypt --passphrase="$RGEE_SECRET" \
---output $HOME/.config/earthengine/ndef/20061abcbc1c6ecf51bd9cf7e37350f6_bmaitner@gmail.com $HOME/emma_envdata/scratch_code/20061abcbc1c6ecf51bd9cf7e37350f6_bmaitner@gmail.com.gpg
+--output $HOME/.config/earthengine/ndef/20061abcbc1c6ecf51bd9cf7e37350f6_bmaitner@gmail.com ./scratch_code/20061abcbc1c6ecf51bd9cf7e37350f6_bmaitner@gmail.com.gpg
 
 gpg --quiet --batch --yes --decrypt --passphrase="$RGEE_SECRET" \
 --output $HOME/.config/earthengine/ndef/credentials $HOME/emma_envdata/scratch_code/credentials.gpg
