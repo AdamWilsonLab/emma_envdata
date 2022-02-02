1994de5126f4b66e7df23ed639a3419e9945db7d
Brian Maitner <bmaitner@gmail.com>
2022-02-02 11:04:51 -0500

    Update decrypt_secret.sh

--- .github/decrypt_secret.sh
+++ .github/decrypt_secret.sh
@@ -10,20 +10,26 @@ mkdir -p /home/rstudio/.config/earthengine/ndef/
 # --batch to prevent interactive command
 # --yes to assume "yes" for questions
 
-gpg --quiet --batch --yes --decrypt --passphrase="$RGEE_SECRET" \
---output $HOME/.config/earthengine/ndef/20061abcbc1c6ecf51bd9cf7e37350f6_bmaitner@gmail.com ./scratch_code/20061abcbc1c6ecf51bd9cf7e37350f6_bmaitner@gmail.com.gpg
+
+# Decrypt ee credentials (currently decrypting to a bunch of places hoping that earth engine finds one)
 
 gpg --quiet --batch --yes --decrypt --passphrase="$RGEE_SECRET" \
 --output $HOME/.config/earthengine/ndef/credentials ./scratch_code/credentials.gpg
-
-gpg --quiet --batch --yes --decrypt --passphrase="$RGEE_SECRET" \
---output /home/rstudio/.config/earthengine/ndef/20061abcbc1c6ecf51bd9cf7e37350f6_bmaitner@gmail.com ./scratch_code/20061abcbc1c6ecf51bd9cf7e37350f6_bmaitner@gmail.com.gpg
 
 gpg --quiet --batch --yes --decrypt --passphrase="$RGEE_SECRET" \
 --output /home/rstudio/.config/earthengine/ndef/credentials ./scratch_code/credentials.gpg
 
 gpg --quiet --batch --yes --decrypt --passphrase="$RGEE_SECRET" \
 --output ~/.config/earthengine/credentials ./scratch_code/credentials.gpg
+
+
+# Decrypt google drive credentials
+gpg --quiet --batch --yes --decrypt --passphrase="$RGEE_SECRET" \
+--output $HOME/.config/earthengine/ndef/20061abcbc1c6ecf51bd9cf7e37350f6_bmaitner@gmail.com ./scratch_code/20061abcbc1c6ecf51bd9cf7e37350f6_bmaitner@gmail.com.gpg
+
+gpg --quiet --batch --yes --decrypt --passphrase="$RGEE_SECRET" \
+--output /home/rstudio/.config/earthengine/ndef/20061abcbc1c6ecf51bd9cf7e37350f6_bmaitner@gmail.com ./scratch_code/20061abcbc1c6ecf51bd9cf7e37350f6_bmaitner@gmail.com.gpg
+
 
 
 
