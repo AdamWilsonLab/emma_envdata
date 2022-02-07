3938ea1d232d14ed7f2ba34c9296e763780b91d0
Brian <bmaitner@gmail.com>
2022-02-07 17:25:29 -0500

    more uncommenting

--- _targets.R
+++ _targets.R
@@ -44,8 +44,7 @@ list(
   tar_target(
     domain,
     domain_define(vegmap = vegmap, country)
-  )
-  #,
+  ),
   # tar_target(
   #   remnants,
   #   domain_remnants(domain, remnants_shp = remnants_shp),
@@ -59,36 +58,37 @@ list(
 
 # Infrequent updates
 
-  # tar_age(
-  #   alos,
-  #   get_alos(domain = domain),
-  #   age = as.difftime(26, units = "weeks")
-  # ),
-  # tar_age(
-  #   climate_chelsa,
-  #   get_climate_chelsa(domain = domain),
-  #   age = as.difftime(26, units = "weeks")
-  # ),
-  # tar_age(
-  #   clouds_wilson,
-  #   get_clouds_wilson(domain = domain),
-  #   age = as.difftime(26, units = "weeks")
-  # ),
-  # tar_age(
-  #   elevation_nasadem,
-  #   get_elevation_nasadem(domain = domain),
-  #   age = as.difftime(26, units = "weeks")
-  # ),
-  # tar_age(
-  #   landcover_za,
-  #   get_landcover_za(domain = domain),
-  #   age = as.difftime(26, units = "weeks")
-  # ),
-  # tar_age(
-  #   precipitation_chelsa,
-  #   get_precipitation_chelsa(domain = domain),
-  #   age = as.difftime(26, units = "weeks")
-  # ),
+  tar_age(
+    alos,
+    get_alos(domain = domain),
+    age = as.difftime(26, units = "weeks")
+  ),
+  tar_age(
+    climate_chelsa,
+    get_climate_chelsa(domain = domain),
+    age = as.difftime(26, units = "weeks")
+  ),
+  tar_age(
+    clouds_wilson,
+    get_clouds_wilson(domain = domain),
+    age = as.difftime(26, units = "weeks")
+  ),
+  tar_age(
+    elevation_nasadem,
+    get_elevation_nasadem(domain = domain),
+    age = as.difftime(26, units = "weeks")
+  ),
+  tar_age(
+    landcover_za,
+    get_landcover_za(domain = domain),
+    age = as.difftime(26, units = "weeks")
+  ),
+  tar_age(
+    precipitation_chelsa,
+    get_precipitation_chelsa(domain = domain),
+    age = as.difftime(26, units = "weeks")
+  )
+  #,
 
 # Frequent updates
 
