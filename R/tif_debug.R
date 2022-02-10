c00b1e0a223b4f0373453065c33ab24fe3b2d616
Brian <bmaitner@gmail.com>
2022-02-10 12:03:19 -0500

    more debugging

--- R/tif_debug.R
+++ R/tif_debug.R
@@ -2,6 +2,9 @@ tif_debug <- function(directory = "scratch_code"){
 
 
   if(file.exists(paste(directory,"/wc2.1_10m_tmin_01.tif",sep = ""))){
+
+    #File found
+
     raster::raster(paste(directory,"/wc2.1_10m_tmin_01.tif",sep = ""))
 
 
@@ -12,9 +15,12 @@ tif_debug <- function(directory = "scratch_code"){
     download.file(url = "https://biogeo.ucdavis.edu/data/worldclim/v2.1/base/wc2.1_10m_tmin.zip",
                   destfile = paste(directory,"/test.zip",sep = ""))
 
+    message("file downloaded")
 
-    unzip(zipfile = paste(directory,"test.zip"),
-          exdir = "scratch_code")
+
+
+    unzip(zipfile = paste(directory,"/test.zip",sep = ""),
+          exdir = "scratch_code",overwrite = TRUE)
 
     raster::raster(paste(directory,"/wc2.1_10m_tmin_01.tif",sep = ""))
 
