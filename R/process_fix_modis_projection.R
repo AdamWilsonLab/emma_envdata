d6feddbee8d7800e676bf5929babb022a33cb3d5
Brian <bmaitner@gmail.com>
2022-02-10 22:18:44 -0500

    updated code to fix modis projection to explicitly use geotiff (just in case)

--- R/process_fix_modis_projection.R
+++ R/process_fix_modis_projection.R
@@ -5,20 +5,20 @@
 
 process_fix_modis_projection <-
 function(directory, ...){
-  
+
   #specify the correct projection
-    nasa_proj <- "+proj=sinu +lon_0=0 +x_0=0 +y_0=0 +R=6371007.181 +units=m +no_defs"  
-  
+    nasa_proj <- "+proj=sinu +lon_0=0 +x_0=0 +y_0=0 +R=6371007.181 +units=m +no_defs"
+
   #get a vector of rasters
     rasters <- list.files(path = directory,
                           pattern = ".tif$",
                           full.names = TRUE)
-    
+
   #set up a  change log if needed
     if(exists(paste(directory,"log.csv",sep = ""))){
-      
-      suppressWarnings(expr = 
-                         cbind("file","original_proj","assigned_proj") %>%  
+
+      suppressWarnings(expr =
+                         cbind("file","original_proj","assigned_proj") %>%
                          write.table(x = .,
                                      file = paste(directory,"log.csv",sep = ""),
                                      append = TRUE,
@@ -27,46 +27,47 @@ function(directory, ...){
                                      sep = ",")
                        )
 
-      
+
     }
-      
-  
+
+
   #iterate and fix
     for(i in 1:length(rasters)){
-      
+
       #load ith raster
         rast_i <- terra::rast(x = rasters[i])
-        
-      #get the projection  
-        
+
+      #get the projection
+
         original_proj <- crs(rast_i, proj = TRUE)
-      
+
       #check whether the raster matches the correct projection
         if(!identical(nasa_proj, original_proj)){
-          
+
           message("Detected error in MODIS projection, correcting and logging the change")
-          
+
           crs(rast_i) <- nasa_proj
-          
+
           #write a new raster with a different name
             terra::writeRaster(x = rast_i,
                                filename = gsub(pattern = ".tif$",
                                                replacement =".temp.tif",
                                                x =  rasters[i]),
+                               filetype="GTiff",
                                overwrite = TRUE)
-          
+
           #delete old raster
             file.remove(rasters[i])
-          
+
           #update new name
             file.rename(from = gsub(pattern = ".tif$",
                                     replacement =".temp.tif",
                                     x =  rasters[i]),
                         to = rasters[i])
-            
-          #log the change  
-            
-            
+
+          #log the change
+
+
           data.frame(file = rasters[i],
                      original_proj = original_proj,
                      assigned_proj = nasa_proj) %>%
@@ -77,17 +78,17 @@ function(directory, ...){
                       col.names = FALSE,
                       row.names=FALSE,
                       sep = ",")
-            
 
-          
+
+
         }
-      
+
     }
-  
-  
-  
+
+
+
 }
 
-  
-  
-  
+
+
+
