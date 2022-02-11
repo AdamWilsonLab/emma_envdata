d36895be8845ba6e356ab0aedd0fbd3f9c5e8b25
Brian <bmaitner@gmail.com>
2022-02-11 13:28:57 -0500

    think bug may be squashed?  adding targets back to see

--- _targets.R
+++ _targets.R
@@ -23,46 +23,47 @@ if(T) {
 
 list(
 
-  tar_age(
-    test_tif,
-    tif_debug(),
-    age = as.difftime(1, units = "secs")
-  )
+  # tar_age(
+  #   test_tif,
+  #   tif_debug(),
+  #   age = as.difftime(1, units = "secs")
+  # )
 
   #Prep needed files
-#
-#   tar_target(
-#     vegmap_shp, # 2018 National Vegetation Map http://bgis.sanbi.org/SpatialDataset/Detail/1674
-#     "data/manual_download/VEGMAP2018_AEA_16082019Final/NVM2018_AEA_V22_7_16082019_final.shp",
-#     format = "file"
-#   ),
-  # tar_target(
-  #   remnants_shp,
-  #   "data/manual_download/RLE_2021_Remnants/RLE_Terr_2021_June2021_Remnants_ddw.shp",
-  #   format = "file"
-  # ),
-  # tar_target(
-  #   country,
-  #   national_boundary()
-  # ),
-  # tar_target(
-  #   vegmap,
-  #   get_vegmap(vegmap_shp)
-  # ),
-  # tar_target(
-  #   domain,
-  #   domain_define(vegmap = vegmap, country)
-  # ),
-  # tar_target(
-  #   remnants,
-  #   domain_remnants(domain, remnants_shp = remnants_shp),
-  #   format = "file"
-  # ),
-  # tar_target(
-  #   remnant_distance,
-  #   domain_distance(remnants),
-  #   format = "file"
-  #  ),
+
+  tar_target(
+    vegmap_shp, # 2018 National Vegetation Map http://bgis.sanbi.org/SpatialDataset/Detail/1674
+    "data/manual_download/VEGMAP2018_AEA_16082019Final/NVM2018_AEA_V22_7_16082019_final.shp",
+    format = "file"
+  ),
+tar_target(
+  remnants_shp,
+  "data/manual_download/RLE_2021_Remnants/RLE_Terr_2021_June2021_Remnants_ddw.shp",
+  format = "file"
+),
+tar_target(
+  country,
+  national_boundary()
+),
+tar_target(
+  vegmap,
+  get_vegmap(vegmap_shp)
+),
+tar_target(
+  domain,
+  domain_define(vegmap = vegmap, country)
+),
+tar_target(
+  remnants,
+  domain_remnants(domain, remnants_shp = remnants_shp),
+  format = "file"
+),
+tar_target(
+  remnant_distance,
+  domain_distance(remnants),
+  format = "file"
+ )
+#,
 
 # Infrequent updates
 
