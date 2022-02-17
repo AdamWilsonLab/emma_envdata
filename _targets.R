9273ae0198b3be5803997ff5e4c0a7d54b97d45a
Brian <bmaitner@gmail.com>
2022-02-17 17:39:11 -0500

    update to readme

--- _targets.R
+++ _targets.R
@@ -58,26 +58,26 @@ tar_target(
     get_climate_chelsa(domain = domain),
     age = as.difftime(26, units = "weeks")
   ),
-  tar_age(
-    clouds_wilson,
-    get_clouds_wilson(domain = domain),
-    age = as.difftime(26, units = "weeks")
-  ),
-  tar_age(
-    elevation_nasadem,
-    get_elevation_nasadem(domain = domain),
-    age = as.difftime(26, units = "weeks")
-  ),
-  tar_age(
-    landcover_za,
-    get_landcover_za(domain = domain),
-    age = as.difftime(26, units = "weeks")
-  ),
-  tar_age(
-    precipitation_chelsa,
-    get_precipitation_chelsa(domain = domain),
-    age = as.difftime(26, units = "weeks")
-  ),
+  # tar_age(
+  #   clouds_wilson,
+  #   get_clouds_wilson(domain = domain),
+  #   age = as.difftime(26, units = "weeks")
+  # ),
+  # tar_age(
+  #   elevation_nasadem,
+  #   get_elevation_nasadem(domain = domain),
+  #   age = as.difftime(26, units = "weeks")
+  # ),
+  # tar_age(
+  #   landcover_za,
+  #   get_landcover_za(domain = domain),
+  #   age = as.difftime(26, units = "weeks")
+  # ),
+  # tar_age(
+  #   precipitation_chelsa,
+  #   get_precipitation_chelsa(domain = domain),
+  #   age = as.difftime(26, units = "weeks")
+  # ),
 
 # # Frequent updates
 #
