916562a433855815a1f2386c8135143885201844
Brian <bmaitner@gmail.com>
2022-02-18 00:18:48 -0500

    targets

--- _targets.R
+++ _targets.R
@@ -68,11 +68,11 @@ tar_target(
     get_elevation_nasadem(domain = domain),
     age = as.difftime(26, units = "weeks")
   ),
-  # tar_age(
-  #   landcover_za,
-  #   get_landcover_za(domain = domain),
-  #   age = as.difftime(26, units = "weeks")
-  # ),
+  tar_age(
+    landcover_za,
+    get_landcover_za(domain = domain),
+    age = as.difftime(26, units = "weeks")
+  ),
   # tar_age(
   #   precipitation_chelsa,
   #   get_precipitation_chelsa(domain = domain),
