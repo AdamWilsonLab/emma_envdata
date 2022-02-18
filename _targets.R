daea7d4487f6102a7868a1d769a41616c912e869
Brian <bmaitner@gmail.com>
2022-02-17 22:09:43 -0500

    targets update

--- _targets.R
+++ _targets.R
@@ -63,16 +63,16 @@ tar_target(
     get_clouds_wilson(domain = domain),
     age = as.difftime(26, units = "weeks")
   ),
-  # tar_age(
-  #   elevation_nasadem,
-  #   get_elevation_nasadem(domain = domain),
-  #   age = as.difftime(26, units = "weeks")
-  # ),
-  # tar_age(
-  #   landcover_za,
-  #   get_landcover_za(domain = domain),
-  #   age = as.difftime(26, units = "weeks")
-  # ),
+  tar_age(
+    elevation_nasadem,
+    get_elevation_nasadem(domain = domain),
+    age = as.difftime(26, units = "weeks")
+  ),
+  tar_age(
+    landcover_za,
+    get_landcover_za(domain = domain),
+    age = as.difftime(26, units = "weeks")
+  ),
   tar_age(
     precipitation_chelsa,
     get_precipitation_chelsa(domain = domain),
