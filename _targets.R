a47656b55e4b83b78a4808fa781dbd0becf04769
Brian <bmaitner@gmail.com>
2022-03-11 10:04:43 -0500

    slowing down the corrections to release projections

--- _targets.R
+++ _targets.R
@@ -174,7 +174,7 @@ tar_target(
     process_fix_modis_release_projection(temp_directory = "data/temp/raw_data/fire_modis/",
                                          tag = "raw_fire_modis",
                                          max_layers = NULL,
-                                         sleep_time = 1,
+                                         sleep_time = 2,
                                  ... = fire_modis_release)
   ),
   tar_target(
@@ -182,7 +182,7 @@ tar_target(
     process_fix_modis_release_projection(temp_directory = "data/temp/raw_data/ndvi_modis/",
                                          tag = "raw_ndvi_modis",
                                          max_layers = NULL,
-                                         sleep_time = 1,
+                                         sleep_time = 2,
                                          ... = ndvi_modis_release)
   ),
 
