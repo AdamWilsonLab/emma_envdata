4e74656757bf725ef9c85c8d0bf581d4af01e8f7
Brian <bmaitner@gmail.com>
2022-03-13 20:38:25 -0400

    temp update to targets

--- _targets.R
+++ _targets.R
@@ -129,16 +129,16 @@ tar_target(
     #age = as.difftime(7, units = "days")
     age = as.difftime(0, units = "hours")
   ),
-  tar_age(
-    ndvi_modis_release,
-    get_release_ndvi_modis(temp_directory = "data/temp/raw_data/ndvi_modis/",
-                            tag = "raw_ndvi_modis",
-                            domain = domain,
-                            max_layers = 30,
-                           sleep_time = 1),
-    #age = as.difftime(7, units = "days")
-    age = as.difftime(0, units = "hours")
-  ),
+  # tar_age(
+  #   ndvi_modis_release,
+  #   get_release_ndvi_modis(temp_directory = "data/temp/raw_data/ndvi_modis/",
+  #                           tag = "raw_ndvi_modis",
+  #                           domain = domain,
+  #                           max_layers = 30,
+  #                          sleep_time = 1),
+  #   #age = as.difftime(7, units = "days")
+  #   age = as.difftime(0, units = "hours")
+  # ),
   tar_age(
     ndvi_dates_modis_release,
     get_release_ndvi_dates_modis(temp_directory = "data/temp/raw_data/ndvi_dates_modis/",
