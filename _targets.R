eec011ce67a171b8ac1186d1e222a659ea6cf7b8
Brian <bmaitner@gmail.com>
2022-03-15 00:37:21 -0400

    increased sleep time

--- _targets.R
+++ _targets.R
@@ -29,23 +29,27 @@ list(
     "data/manual_download/VEGMAP2018_AEA_16082019Final/NVM2018_AEA_V22_7_16082019_final.shp",
     format = "file"
   ),
-tar_target(
-  remnants_shp,
-  "data/manual_download/RLE_2021_Remnants/RLE_Terr_2021_June2021_Remnants_ddw.shp",
-  format = "file"
-),
-tar_target(
-  country,
-  national_boundary()
-),
-tar_target(
-  vegmap,
-  get_vegmap(vegmap_shp)
-),
-tar_target(
-  domain,
-  domain_define(vegmap = vegmap, country)
-),
+
+  tar_target(
+    remnants_shp,
+    "data/manual_download/RLE_2021_Remnants/RLE_Terr_2021_June2021_Remnants_ddw.shp",
+    format = "file"
+  ),
+
+  tar_target(
+    country,
+    national_boundary()
+  ),
+
+  tar_target(
+    vegmap,
+    get_vegmap(vegmap_shp)
+  ),
+
+  tar_target(
+    domain,
+    domain_define(vegmap = vegmap, country)
+  ),
 
 # Infrequent updates
 
@@ -54,125 +58,85 @@ tar_target(
     get_alos(domain = domain),
     age = as.difftime(26, units = "weeks")
   ),
+
   tar_age(
     climate_chelsa,
     get_climate_chelsa(domain = domain),
     age = as.difftime(26, units = "weeks")
   ),
+
   tar_age(
     clouds_wilson,
     get_clouds_wilson(domain = domain),
     age = as.difftime(26, units = "weeks")
   ),
+
   tar_age(
     elevation_nasadem,
     get_elevation_nasadem(domain = domain),
     age = as.difftime(26, units = "weeks")
   ),
+
   # tar_age(
   #   landcover_za,
   #   get_landcover_za(domain = domain),
   #   age = as.difftime(26, units = "weeks")
   # ),
+
   tar_age(
     precipitation_chelsa,
     get_precipitation_chelsa(domain = domain),
     age = as.difftime(26, units = "weeks")
   ),
 
-# # Frequent updates
+# Frequent updates via releases
 
-  tar_age(
-    fire_modis,
-    get_fire_modis(domain = domain,
-                   max_layers = 50),
-    age = as.difftime(7, units = "days")
-  ),
-  tar_age(
-    kndvi_modis,
-    get_kndvi_modis(domain = domain,
-                    max_layers = 50),
-    age = as.difftime(7, units = "days")
-    #age = as.difftime(0, units = "hours")
-  ),
-  tar_age(
-    ndvi_modis,
-    get_ndvi_modis(domain = domain,
-                   max_layers = 50),
-    age = as.difftime(7, units = "days")
-  ),
-  tar_age(
-    ndvi_dates_modis,
-    get_ndvi_dates_modis(domain = domain,
-                         max_layers = 50),
-    age = as.difftime(7, units = "days")
-    #age = as.difftime(0, units = "hours")
-  ),
-  # Frequent updates via releases
     tar_age(
       fire_modis_release,
       get_release_fire_modis(temp_directory = "data/temp/raw_data/fire_modis/",
                              tag = "raw_fire_modis",
                              domain = domain,
                              max_layers = 50,
-                             sleep_time = 1),
+                             sleep_time = 5),
       age = as.difftime(7, units = "days")
       #age = as.difftime(0, units = "hours")
     ),
+
     tar_age(
     kndvi_modis_release,
     get_release_kndvi_modis(temp_directory = "data/temp/raw_data/kndvi_modis/",
                            tag = "raw_kndvi_modis",
                            domain = domain,
                            max_layers = 50,
-                           sleep_time = 1),
+                           sleep_time = 5),
     age = as.difftime(7, units = "days")
     #age = as.difftime(0, units = "hours")
   ),
+
   tar_age(
     ndvi_modis_release,
     get_release_ndvi_modis(temp_directory = "data/temp/raw_data/ndvi_modis/",
                             tag = "raw_ndvi_modis",
                             domain = domain,
                             max_layers = 50,
-                           sleep_time = 1),
+                           sleep_time = 5),
     age = as.difftime(7, units = "days")
     #age = as.difftime(0, units = "hours")
   ),
+
   tar_age(
     ndvi_dates_modis_release,
     get_release_ndvi_dates_modis(temp_directory = "data/temp/raw_data/ndvi_dates_modis/",
                            tag = "raw_ndvi_dates_modis",
                            domain = domain,
                            max_layers = 50,
-                           sleep_time = 1),
+                           sleep_time = 5),
     #age = as.difftime(7, units = "days")
     age = as.difftime(0, units = "hours")
   ),
 
-# Fixing projections
-
-  tar_target(
-    correct_ndvi_proj,
-    process_fix_modis_projection(directory = "data/raw_data/ndvi_modis/",
-                                 ... = ndvi_modis)
-  ),
-  tar_target(
-    correct_ndvi_date_proj,
-    process_fix_modis_projection(directory = "data/raw_data/ndvi_dates_modis/",
-                               ... = ndvi_dates_modis)
-  ),
-  tar_target(
-    correct_kndvi_proj,
-    process_fix_modis_projection(directory = "data/raw_data/kndvi_modis/",
-                               ... = kndvi_modis)
-  ),
-  tar_target(
-    correct_fire_proj,
-    process_fix_modis_projection(directory = "data/raw_data/fire_modis/",
-                               ... = fire_modis)
-  ),
 # Fixing projection via releases
+
   tar_target(
     correct_fire_release_proj,
     process_fix_modis_release_projection(temp_directory = "data/temp/raw_data/fire_modis/",
@@ -206,29 +170,17 @@ tar_target(
                                          ... = kndvi_modis_release)
   ),
 
-#
-# # Processing
 
-    tar_target(
-      fire_doy_to_unix_date_release,
-      process_release_fire_doy_to_unix_date(input_tag = "raw_fire_modis",
-                                            output_tag = "processed_fire_dates",
-                                            temp_directory = "data/temp/processed_data/fire_dates/",
-                                            sleep_time = 5,
-                                            ... = correct_fire_release_proj)
-    ),
-
-    tar_target(
-      fire_doy_to_unix_date,
-      process_fire_doy_to_unix_date(... = correct_fire_proj)
-    ),
-
-  ##
+# Processing via release
 
   tar_target(
-    burn_date_to_last_burned_date,
-    process_burn_date_to_last_burned_date(... = fire_doy_to_unix_date)
-  ),
+    fire_doy_to_unix_date_release,
+    process_release_fire_doy_to_unix_date(input_tag = "raw_fire_modis",
+                                          output_tag = "processed_fire_dates",
+                                          temp_directory = "data/temp/processed_data/fire_dates/",
+                                          sleep_time = 5,
+                                          ... = correct_fire_release_proj)
+    ),
 
   tar_target(
     burn_date_to_last_burned_date_release,
@@ -240,13 +192,6 @@ tar_target(
                                                   ... = fire_doy_to_unix_date_release)
   ),
 
-  ##
-
-  tar_target(
-    ndvi_relative_days_since_fire,
-    process_ndvi_relative_days_since_fire(... = burn_date_to_last_burned_date,
-                                          ... = correct_ndvi_date_proj)
-  ),
 
   tar_target(
     ndvi_relative_days_since_fire_release,
@@ -268,10 +213,12 @@ tar_target(
 #     get_model_data(remnant_distance),
 #     format = "file"
 #   ),
+
   tar_target(
     template,
     get_template_raster(... = correct_ndvi_proj)
   ),
+
   tar_target(
     remnants,
     domain_remnants(domain = domain,
@@ -280,6 +227,7 @@ tar_target(
                     file = "data/processed_data/remnants/remnants.tif"),
     format = "file"
   ),
+
   tar_target(
     remnant_distance,
     domain_distance(remnants,
@@ -291,22 +239,27 @@ tar_target(
 #     projected_alos,
 #     process_alos(template = template, ... = alos)
 #   ),
+#
 #   tar_target(
 #     projected_climate_chelsa,
 #     process_climate_chelsa(template = template, ... = climate_chelsa)
 #   ),
+#
 #   tar_target(
 #     projected_clouds_wilson,
 #     process_clouds_wilson(template = template, ... = clouds_wilson)
 #   ),
+#
 #   tar_target(
 #     projected_elevation_nasadem,
 #     process_elevation_nasadem(template = template, ... = elevation_nasadem)
 #   ),
+#
 #   tar_target(
 #     projected_landcover_za,
 #     process_landcover_za(template = template, ... = landcover_za)
 #   ),
+#
 #   tar_target(
 #     projected_precipitation_chelsa,
 #     process_precipitation_chelsa(template = template, ... = precipitaton_chelsa)
@@ -366,3 +319,75 @@ tar_target(
 # )
 
 )
+
+##############################
+
+#archived bits below
+
+# # Frequent updates
+#
+# tar_age(
+#   fire_modis,
+#   get_fire_modis(domain = domain,
+#                  max_layers = 50),
+#   age = as.difftime(7, units = "days")
+# ),
+# tar_age(
+#   kndvi_modis,
+#   get_kndvi_modis(domain = domain,
+#                   max_layers = 50),
+#   age = as.difftime(7, units = "days")
+#   #age = as.difftime(0, units = "hours")
+# ),
+# tar_age(
+#   ndvi_modis,
+#   get_ndvi_modis(domain = domain,
+#                  max_layers = 50),
+#   age = as.difftime(7, units = "days")
+# ),
+# tar_age(
+#   ndvi_dates_modis,
+#   get_ndvi_dates_modis(domain = domain,
+#                        max_layers = 50),
+#   age = as.difftime(7, units = "days")
+#   #age = as.difftime(0, units = "hours")
+# ),
+## Fixing projections
+#
+# tar_target(
+#   correct_ndvi_proj,
+#   process_fix_modis_projection(directory = "data/raw_data/ndvi_modis/",
+#                                ... = ndvi_modis)
+# ),
+# tar_target(
+#   correct_ndvi_date_proj,
+#   process_fix_modis_projection(directory = "data/raw_data/ndvi_dates_modis/",
+#                                ... = ndvi_dates_modis)
+# ),
+# tar_target(
+#   correct_kndvi_proj,
+#   process_fix_modis_projection(directory = "data/raw_data/kndvi_modis/",
+#                                ... = kndvi_modis)
+# ),
+# tar_target(
+#   correct_fire_proj,
+#   process_fix_modis_projection(directory = "data/raw_data/fire_modis/",
+#                                ... = fire_modis)
+# ),
+## Processing
+
+# tar_target(
+#   fire_doy_to_unix_date,
+#   process_fire_doy_to_unix_date(... = correct_fire_proj)
+# ),
+#
+# tar_target(
+#   burn_date_to_last_burned_date,
+#   process_burn_date_to_last_burned_date(... = fire_doy_to_unix_date)
+# ),
+# tar_target(
+#   ndvi_relative_days_since_fire,
+#   process_ndvi_relative_days_since_fire(... = burn_date_to_last_burned_date,
+#                                         ... = correct_ndvi_date_proj)
+# ),
+
