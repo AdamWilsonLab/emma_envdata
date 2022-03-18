da8d9e3a85b7234135a0333712018d06a4ee0374
Brian <bmaitner@gmail.com>
2022-03-18 17:16:44 -0400

    remove note to self from targets

--- _targets.R
+++ _targets.R
@@ -172,7 +172,6 @@ tar_age(
   ),
   tar_target(
     correct_ndvi_release_proj,
-    stop("BRIAN: 2013-02-1 through 2013-04-01 may need to be re-corrected.  DOuble check")
     process_fix_modis_release_projection(temp_directory = "data/temp/raw_data/ndvi_modis/",
                                          tag = "raw_ndvi_modis",
                                          max_layers = NULL,
