2c36f5c19d925cc46f7519975af575b43f3a6dba
Brian <bmaitner@gmail.com>
2022-03-21 09:37:58 -0400

    targets

--- _targets.R
+++ _targets.R
@@ -104,62 +104,64 @@ list(
     age = as.difftime(62, units = "weeks")
   ),
 
-tar_age(
-  soil_gcfr_release,
-  get_release_soil_gcfr(temp_directory = "data/temp/raw_data/soil_gcfr/",
-                        tag = "raw_static",
-                        domain),
-  age = as.difftime(64, units = "weeks")
-)
-#,
-#
-#
-#
+# tar_age(
+#   soil_gcfr_release,
+#   get_release_soil_gcfr(temp_directory = "data/temp/raw_data/soil_gcfr/",
+#                         tag = "raw_static",
+#                         domain),
+#   age = as.difftime(64, units = "weeks")
+# ),
+
+
+
+
+
 # # Frequent updates via releases
-#
-#     tar_age(
-#       fire_modis_release,
-#       get_release_fire_modis(temp_directory = "data/temp/raw_data/fire_modis/",
-#                              tag = "raw_fire_modis",
-#                              domain = domain,
-#                              max_layers = 25,
-#                              sleep_time = 5),
-#       age = as.difftime(7, units = "days")
-#       #age = as.difftime(0, units = "hours")
-#     ),
-#
-#     tar_age(
-#     kndvi_modis_release,
-#     get_release_kndvi_modis(temp_directory = "data/temp/raw_data/kndvi_modis/",
-#                            tag = "raw_kndvi_modis",
-#                            domain = domain,
-#                            max_layers = 25,
-#                            sleep_time = 5),
-#     age = as.difftime(7, units = "days")
-#     #age = as.difftime(0, units = "hours")
-#   ),
-#
-#   tar_age(
-#     ndvi_modis_release,
-#     get_release_ndvi_modis(temp_directory = "data/temp/raw_data/ndvi_modis/",
-#                             tag = "raw_ndvi_modis",
-#                             domain = domain,
-#                             max_layers = 25,
-#                            sleep_time = 5),
-#     age = as.difftime(7, units = "days")
-#     #age = as.difftime(0, units = "hours")
-#   ),
-#
-#   tar_age(
-#     ndvi_dates_modis_release,
-#     get_release_ndvi_dates_modis(temp_directory = "data/temp/raw_data/ndvi_dates_modis/",
-#                            tag = "raw_ndvi_dates_modis",
-#                            domain = domain,
-#                            max_layers = 25,
-#                            sleep_time = 10),
-#     #age = as.difftime(7, units = "days")
-#     age = as.difftime(0, units = "hours")
-#   ),
+
+    tar_age(
+      fire_modis_release,
+      get_release_fire_modis(temp_directory = "data/temp/raw_data/fire_modis/",
+                             tag = "raw_fire_modis",
+                             domain = domain,
+                             max_layers = 25,
+                             sleep_time = 5),
+      age = as.difftime(7, units = "days")
+      #age = as.difftime(0, units = "hours")
+    ),
+
+    tar_age(
+    kndvi_modis_release,
+    get_release_kndvi_modis(temp_directory = "data/temp/raw_data/kndvi_modis/",
+                           tag = "raw_kndvi_modis",
+                           domain = domain,
+                           max_layers = 25,
+                           sleep_time = 5),
+    age = as.difftime(7, units = "days")
+    #age = as.difftime(0, units = "hours")
+  ),
+
+  tar_age(
+    ndvi_modis_release,
+    get_release_ndvi_modis(temp_directory = "data/temp/raw_data/ndvi_modis/",
+                            tag = "raw_ndvi_modis",
+                            domain = domain,
+                            max_layers = 25,
+                           sleep_time = 5),
+    age = as.difftime(7, units = "days")
+    #age = as.difftime(0, units = "hours")
+  ),
+
+  tar_age(
+    ndvi_dates_modis_release,
+    get_release_ndvi_dates_modis(temp_directory = "data/temp/raw_data/ndvi_dates_modis/",
+                           tag = "raw_ndvi_dates_modis",
+                           domain = domain,
+                           max_layers = 25,
+                           sleep_time = 10),
+    #age = as.difftime(7, units = "days")
+    age = as.difftime(0, units = "hours")
+  )
+#,
 #
 # # Fixing projection via releases
 #
