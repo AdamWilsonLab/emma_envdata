c3077ba7241c508cf404556317d6f2ed20fd4916
Brian <bmaitner@gmail.com>
2022-03-28 01:32:19 -0400

    alternate format for cloud release

--- R/process_release_clouds_wilson.R
+++ R/process_release_clouds_wilson.R
@@ -13,6 +13,7 @@ process_release_clouds_wilson <- function(input_tag = "raw_static",
 
 
 
+
   #  #Ensure directory is empty if it exists
 
     if(dir.exists(temp_directory)){
@@ -81,6 +82,8 @@ process_release_clouds_wilson <- function(input_tag = "raw_static",
                               overwrite=TRUE
         )
 
+        rm(raster_i)
+
         #Terra is currently having some problems with reading and writing so I've switched back to raster for now
         # terra::project(x = raster_i,
         #                y = template,
@@ -88,21 +91,18 @@ process_release_clouds_wilson <- function(input_tag = "raw_static",
         #                filename = file.path(temp_directory, paste("temp_",raster_list$file_name[i],sep = "")),
         #                overwrite = TRUE)
 
+      } #i loop
 
-        pb_upload(file = file.path(temp_directory, raster_list$file_name[i]),
+
+        pb_upload(file = file.path(temp_directory, raster_list$file_name),
                   repo = "AdamWilsonLab/emma_envdata",
                   tag = output_tag,
                   name = raster_list$file_name[i])
 
-        rm(raster_i)
 
-        file.remove(file.path(temp_directory, raster_list$file_name[i]))
-
-        #Pause to keep below the rate limit
-          Sys.sleep(sleep_time)
+        file.remove(file.path(temp_directory, raster_list$file_name))
 
 
-      } #i loop
 
   #Clear out the folder
 
@@ -120,3 +120,115 @@ process_release_clouds_wilson <- function(input_tag = "raw_static",
 
 #################################
 
+# process_release_clouds_wilson <- function(input_tag = "raw_static",
+#                                           output_tag = "processed_static",
+#                                           temp_directory = "data/temp/raw_data/clouds_wilson/",
+#                                           template_release,
+#                                           sleep_time = 30,
+#                                           ...){
+#
+#
+#
+#   #  #Ensure directory is empty if it exists
+#
+#   if(dir.exists(temp_directory)){
+#     unlink(file.path(temp_directory), recursive = TRUE, force = TRUE)
+#   }
+#
+#   # make a directory if one doesn't exist yet
+#
+#   if(!dir.exists(temp_directory)){
+#     dir.create(temp_directory, recursive = TRUE)
+#   }
+#
+#
+#
+#   # get template raster
+#
+#   robust_pb_download(file = template_release$file,
+#                      dest = temp_directory,
+#                      repo = template_release$repo,
+#                      tag = template_release$tag,
+#                      max_attempts = 10,
+#                      sleep_time = 10)
+#
+#   #Pause to keep below the rate limit
+#   Sys.sleep(sleep_time)
+#
+#   #template <- terra::rast(file.path(temp_directory, template_release$file))
+#   template <- raster::raster(file.path(temp_directory, template_release$file))
+#
+#
+#   # get input rasters
+#
+#   raster_list <- pb_list(repo = "AdamWilsonLab/emma_envdata",
+#                          tag = input_tag) %>%
+#     filter(grepl(pattern = "MODCF",
+#                  x = file_name))
+#
+#   robust_pb_download(file = raster_list$file_name,
+#                      dest = temp_directory,
+#                      repo = "AdamWilsonLab/emma_envdata",
+#                      tag = input_tag,
+#                      max_attempts = 10,
+#                      sleep_time = sleep_time)
+#
+#   #Pause to keep below the rate limit
+#   Sys.sleep(sleep_time)
+#
+#
+#   #reformat and save each
+#
+#   for(i in 1:nrow(raster_list)){
+#
+#     raster_i <- raster::raster(file.path(temp_directory, raster_list$file_name[i]))
+#     #raster_i <- terra::rast(file.path(temp_directory, raster_list$file_name[i]))
+#
+#
+#     #Use bilinear for everything
+#
+#     method <- "bilinear"
+#
+#
+#     raster::projectRaster(from = raster_i,
+#                           to = template,
+#                           method = method,
+#                           filename = file.path(temp_directory, raster_list$file_name[i],sep = ""),
+#                           overwrite=TRUE
+#     )
+#
+#     #Terra is currently having some problems with reading and writing so I've switched back to raster for now
+#     # terra::project(x = raster_i,
+#     #                y = template,
+#     #                method = method,
+#     #                filename = file.path(temp_directory, paste("temp_",raster_list$file_name[i],sep = "")),
+#     #                overwrite = TRUE)
+#
+#
+#     pb_upload(file = file.path(temp_directory, raster_list$file_name[i]),
+#               repo = "AdamWilsonLab/emma_envdata",
+#               tag = output_tag,
+#               name = raster_list$file_name[i])
+#
+#     rm(raster_i)
+#
+#     file.remove(file.path(temp_directory, raster_list$file_name[i]))
+#
+#     #Pause to keep below the rate limit
+#     Sys.sleep(sleep_time)
+#
+#
+#   } #i loop
+#
+#   #Clear out the folder
+#
+#   unlink(file.path(temp_directory), recursive = TRUE, force = TRUE)
+#
+#   #End functions
+#
+#   message("Finished processing cloud layers")
+#   return(invisible(NULL))
+#
+#
+# } #end fx
+#
