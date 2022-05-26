03c2fee88c300a8b04acbda2c8a04d4f4be746e0
Brian <bmaitner@gmail.com>
2022-05-26 11:14:23 -0400

    commenting

--- _targets.R
+++ _targets.R
@@ -353,20 +353,20 @@ tar_target(
 ),
 
 # # # Prep model data
-  tar_target(
-    stable_data_release,
-    process_release_stable_data(temp_directory = "data/temp/processed_data/static/",
-                                input_tag = "processed_static",
-                                output_tag = "current",
-                                ... = projected_precipitation_chelsa_release,
-                                ... = projected_landcover_za_release,
-                                ... = projected_elevation_nasadem_release,
-                                ... = projected_clouds_wilson_release,
-                                ... = projected_climate_chelsa_release,
-                                ... = projected_alos_release,
-                                ... = remnant_distance_release,
-                                ... = projected_soil_gcfr_release)
-    ),
+  # tar_target(
+  #   stable_data_release,
+  #   process_release_stable_data(temp_directory = "data/temp/processed_data/static/",
+  #                               input_tag = "processed_static",
+  #                               output_tag = "current",
+  #                               ... = projected_precipitation_chelsa_release,
+  #                               ... = projected_landcover_za_release,
+  #                               ... = projected_elevation_nasadem_release,
+  #                               ... = projected_clouds_wilson_release,
+  #                               ... = projected_climate_chelsa_release,
+  #                               ... = projected_alos_release,
+  #                               ... = remnant_distance_release,
+  #                               ... = projected_soil_gcfr_release)
+  #   ),
 
   tar_target(
     ndvi_to_parquet_release,
