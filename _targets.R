154b62cd6659cd1fb3fec65cee2e8de7b406ad66
Brian <bmaitner@gmail.com>
2022-06-24 11:53:50 -0400

    targets

--- _targets.R
+++ _targets.R
@@ -102,22 +102,22 @@ list(
     age = as.difftime(58, units = "weeks")
   ),
 
-  # tar_age(
-  #   landcover_za_release,
-  #   get_release_landcover_za(temp_directory = "data/temp/raw_data/landcover_za/",
-  #                            tag = "raw_static",
-  #                            domain = domain),
-  #   age = as.difftime(60, units = "weeks")
-  # ),
-  #
-  # tar_age(
-  #   precipitation_chelsa_release,
-  #   get_release_precipitation_chelsa(temp_directory = "data/temp/raw_data/precipitation_chelsa/",
-  #                                    tag = "raw_static",
-  #                                    domain = domain),
-  #   age = as.difftime(62, units = "weeks")
-  # ),
-  #
+  tar_age(
+    landcover_za_release,
+    get_release_landcover_za(temp_directory = "data/temp/raw_data/landcover_za/",
+                             tag = "raw_static",
+                             domain = domain),
+    age = as.difftime(60, units = "weeks")
+  ),
+
+  tar_age(
+    precipitation_chelsa_release,
+    get_release_precipitation_chelsa(temp_directory = "data/temp/raw_data/precipitation_chelsa/",
+                                     tag = "raw_static",
+                                     domain = domain),
+    age = as.difftime(62, units = "weeks")
+  ),
+
   # tar_age(
   #   soil_gcfr_release,
   #   get_release_soil_gcfr(temp_directory = "data/temp/raw_data/soil_gcfr/",
