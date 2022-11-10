6f3734b19c1ae33b03b696a2d6192ae9b0d80b0d
Brian <bmaitner@gmail.com>
2022-11-10 15:05:41 -0500

    added code for rasterizing vegmap

--- _targets.R
+++ _targets.R
@@ -391,6 +391,18 @@ list(
 
       ),
 
+      tar_target(
+        vegmap_modis_proj,
+        process_release_biome_raster(template_release = template_release,
+                                     vegmap_shp = vegmap_shp,
+                                     temp_directory = "data/temp/raw_data/vegmap_raster/",
+                                     sleep_time = 10)
+
+      ),
+
+
+
+
 # # # # Prep model data
 
       tar_target(
