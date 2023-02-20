16902770eacc530d0bda1aa6af197994b56d997a
Brian <bmaitner@gmail.com>
2023-02-20 12:26:02 -0500

    fiddling with decryption

--- .github/decrypt_secret.sh
+++ .github/decrypt_secret.sh
@@ -37,7 +37,9 @@ gpg --quiet --batch --yes --decrypt --passphrase="$RGEE_SECRET" \
 
 # Decrypt google drive credentials (newer version)
 gpg --quiet --batch --yes --decrypt --passphrase="$RGEE_SECRET" \
---output ./scratch_code/maitner-f590bfc7be54.json ./scratch_code/maitner-f590bfc7be54.json.gpg
+--output /home/rstudio/.config/earthengine/ndef/maitner-f590bfc7be54.json ./scratch_code/maitner-f590bfc7be54.json.gpg
 
+gpg --quiet --batch --yes --decrypt --passphrase="$RGEE_SECRET" \
+--output $HOME/.config/earthengine/ndef/maitner-f590bfc7be54.json ./scratch_code/maitner-f590bfc7be54.json.gpg
 
 
