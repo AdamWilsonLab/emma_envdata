ce414e391392d5768a281ffe0a75f3c63c078883
Brian <bmaitner@gmail.com>
2023-05-03 17:26:12 -0400

    forgot to update the token

--- _targets.R
+++ _targets.R
@@ -20,7 +20,7 @@ tar_option_set(packages = c("cmdstanr", "posterior", "bayesplot", "tidyverse",
                             "cubelyr","rgee"))
 
 #set JSON token location (should be authorized for drive and earth engine)
-json_token <- "secrets/ee-wilsonlab-emma-e7d8f39311f1.json"
+json_token <- "secrets/ee-wilsonlab-emma-ef416058504a.json"
 
 # ee authentication
 if(T) {
