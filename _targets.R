83d50b508d4b69353492a6c0ccafcecb99bd66d1
Brian <bmaitner@gmail.com>
2023-05-04 17:09:23 -0400

    uncommenting

--- _targets.R
+++ _targets.R
@@ -129,14 +129,14 @@ list(
   #   age = as.difftime(60, units = "weeks")
   # ),
 
-  # tar_age(
-  #   precipitation_chelsa_release,
-  #   get_release_precipitation_chelsa(temp_directory = "data/temp/raw_data/precipitation_chelsa/",
-  #                                    tag = "raw_static",
-  #                                    domain = domain),
-  #   age = as.difftime(62, units = "weeks")
-  # ),
-  #
+  tar_age(
+    precipitation_chelsa_release,
+    get_release_precipitation_chelsa(temp_directory = "data/temp/raw_data/precipitation_chelsa/",
+                                     tag = "raw_static",
+                                     domain = domain),
+    age = as.difftime(62, units = "weeks")
+  ),
+
   # tar_age(
   #   soil_gcfr_release,
   #   get_release_soil_gcfr(temp_directory = "data/temp/raw_data/soil_gcfr/",
