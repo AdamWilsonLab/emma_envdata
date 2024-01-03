96c41a4e3cb69920863252a4fbdd2146b4d414ad
Brian <bmaitner@gmail.com>
2024-01-03 14:52:29 -0500

    removed commented raster calls

--- R/process_release_elevation_nasadem.R
+++ R/process_release_elevation_nasadem.R
@@ -1,7 +1,7 @@
 #' @author Brian Maitner
 #' @param input_tag tag associated with input files
 #' @param output_tag tag assocaited with output files
-#' @param temp_directory A temporary directory to use for processessing.  Will be cleared out at the end.
+#' @param temp_directory A temporary directory to use for processing.  Will be cleared out at the end.
 #' @param template_release path to raster file to use as a template for reprojection
 #' @param ... Does nothing, but is used in making connections between files in the targets framework
 #' @note This function uses method = bilinear at present
@@ -35,8 +35,6 @@ process_release_elevation_nasadem <- function(input_tag = "raw_static",
                        sleep_time = sleep_time)
 
     template <- terra::rast(file.path(temp_directory, template_release$file))
-    #template <- raster::raster(file.path(temp_directory, template_release$file))
-
 
     # get input rasters
 
@@ -57,33 +55,21 @@ process_release_elevation_nasadem <- function(input_tag = "raw_static",
 
     for(i in 1:nrow(raster_list)){
 
-      #raster_i <- raster::raster(file.path(temp_directory, raster_list$file_name[i]))
       raster_i <- terra::rast(file.path(temp_directory, raster_list$file_name[i]))
-
 
       #Use bilinear for everything
 
         method <- "bilinear"
 
-      # raster::projectRaster(from = raster_i,
-      #                       to = template,
-      #                       method = method,
-      #                       filename = file.path(temp_directory, paste("tf_",raster_list$file_name[i],sep = "")),
-      #                       overwrite = TRUE
-      #                       )
-
-      #Terra is currently having some problems with reading and writing so I've switched back to raster for now
         #ok, oddly enough terra resample seems to fail on github, but work locally.
         #even weirder, terra project seems to work on both
 
-    #temp_i <-
       terra::project(x = raster_i,
                      y = template,
                      method = method,
                      filename = file.path(temp_directory, paste("temp_",raster_list$file_name[i],sep = "")),
                      overwrite = TRUE)
 
-      # tf_i <-
       # terra::resample(x = raster_i,
       #                y = template,
       #                method = method,
