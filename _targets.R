a8ac1d6e469aa6f499373d31e1f2911b39a68843
Brian <bmaitner@gmail.com>
2024-01-03 11:36:57 -0500

    uncommenting

--- _targets.R
+++ _targets.R
@@ -119,29 +119,29 @@ list(
                               sleep_time = 180)
     ),
 
-  # tar_age(
-  #   elevation_nasadem_release,
-  #   get_release_elevation_nasadem(temp_directory = "data/temp/raw_data/elevation_nasadem/",
-  #                                 tag = "raw_static",
-  #                                 domain),
-  #   age = as.difftime(58, units = "weeks")
-  # ),
-  #
-  # tar_age(
-  #   landcover_za_release,
-  #   get_release_landcover_za(temp_directory = "data/temp/raw_data/landcover_za/",
-  #                            tag = "raw_static",
-  #                            domain = domain),
-  #   age = as.difftime(60, units = "weeks")
-  # ),
-  #
-  # tar_age(
-  #   precipitation_chelsa_release,
-  #   get_release_precipitation_chelsa(temp_directory = "data/temp/raw_data/precipitation_chelsa/",
-  #                                    tag = "raw_static",
-  #                                    domain = domain),
-  #   age = as.difftime(62, units = "weeks")
-  # ),
+  tar_age(
+    elevation_nasadem_release,
+    get_release_elevation_nasadem(temp_directory = "data/temp/raw_data/elevation_nasadem/",
+                                  tag = "raw_static",
+                                  domain),
+    age = as.difftime(58, units = "weeks")
+  ),
+
+  tar_age(
+    landcover_za_release,
+    get_release_landcover_za(temp_directory = "data/temp/raw_data/landcover_za/",
+                             tag = "raw_static",
+                             domain = domain),
+    age = as.difftime(60, units = "weeks")
+  ),
+
+  tar_age(
+    precipitation_chelsa_release,
+    get_release_precipitation_chelsa(temp_directory = "data/temp/raw_data/precipitation_chelsa/",
+                                     tag = "raw_static",
+                                     domain = domain),
+    age = as.difftime(62, units = "weeks")
+  ),
   #
   # tar_target(
   #   soil_gcfr_release,
