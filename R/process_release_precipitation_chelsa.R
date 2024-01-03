65720b73c674b75dd54ff37db6621298499186a6
Brian <bmaitner@gmail.com>
2024-01-03 14:45:56 -0500

    removed raster dependency

--- R/process_release_precipitation_chelsa.R
+++ R/process_release_precipitation_chelsa.R
@@ -34,30 +34,30 @@ process_release_precipitation_chelsa <- function(input_tag = "raw_static",
                        sleep_time = 10)
 
     #Pause to keep below the rate limit
+
       Sys.sleep(sleep_time)
 
-    #template <- terra::rast(file.path(temp_directory, template_release$file))
-    template <- raster::raster(file.path(temp_directory, template_release$file))
-
+      template <- terra::rast(file.path(temp_directory, template_release$file))
 
   # get input rasters
 
-  raster_list <- pb_list(repo = "AdamWilsonLab/emma_envdata",
-                         tag = input_tag) %>%
-    filter(grepl(pattern = "CHELSA_prec",
-                 x = file_name)) %>%
-    filter(grepl(pattern = ".tif",
-                 x = file_name))
+    raster_list <- pb_list(repo = "AdamWilsonLab/emma_envdata",
+                           tag = input_tag) %>%
+      filter(grepl(pattern = "CHELSA_prec",
+                   x = file_name)) %>%
+      filter(grepl(pattern = ".tif",
+                   x = file_name))
 
-  robust_pb_download(file = raster_list$file_name,
-                     dest = temp_directory,
-                     repo = "AdamWilsonLab/emma_envdata",
-                     tag = input_tag,
-                     max_attempts = 10,
-                     sleep_time = sleep_time)
+    robust_pb_download(file = raster_list$file_name,
+                       dest = temp_directory,
+                       repo = "AdamWilsonLab/emma_envdata",
+                       tag = input_tag,
+                       max_attempts = 10,
+                       sleep_time = sleep_time)
 
   #Pause to keep below the rate limit
-  Sys.sleep(sleep_time)
+
+    Sys.sleep(sleep_time)
 
 
   #reformat and save each
@@ -65,45 +65,33 @@ process_release_precipitation_chelsa <- function(input_tag = "raw_static",
 
   for(i in 1:nrow(raster_list)){
 
-    raster_i <- raster::raster(file.path(temp_directory, raster_list$file_name[i]))
-    #raster_i <- terra::rast(file.path(temp_directory, raster_list$file_name[i]))
-
+    raster_i <- terra::rast(file.path(temp_directory, raster_list$file_name[i]))
 
     #Use bilinear for everything
 
-    method <- "bilinear"
+      method <- "bilinear"
 
-    #Unfortunately I have to save the raster as a new file as it fails to overwrite occaisionally via actions
-    raster::projectRaster(from = raster_i,
-                          to = template,
-                          method = method,
-                          filename = file.path(temp_directory, paste("tf_",raster_list$file_name[i],sep = "")),
-                          overwrite=TRUE
-                          )
-
-    #Terra is currently having some problems with reading and writing so I've switched back to raster for now
-    # terra::project(x = raster_i,
-    #                y = template,
-    #                method = method,
-    #                filename = file.path(temp_directory, paste("temp_",raster_list$file_name[i],sep = "")),
-    #                overwrite = TRUE)
+      terra::project(x = raster_i,
+                     y = template,
+                     method = method,
+                     filename = file.path(temp_directory, paste("temp_",raster_list$file_name[i],sep = "")),
+                     overwrite = TRUE)
 
 
-    if(projection(raster(file.path(temp_directory, paste("tf_",raster_list$file_name[i],sep = "")))) != projection(template)){stop("Issue with reprojection")}
+    if(crs(rast(file.path(temp_directory, paste("temp_",raster_list$file_name[i],sep = ""))),proj=TRUE)
+       != crs(template,proj = TRUE)){stop("Issue with reprojection")}
 
 
 
-    pb_upload(file = file.path(temp_directory, paste("tf_",raster_list$file_name[i],sep = "")),
+    pb_upload(file = file.path(temp_directory, paste("temp_",raster_list$file_name[i],sep = "")),
               repo = "AdamWilsonLab/emma_envdata",
               tag = output_tag,
               name = raster_list$file_name[i])
 
-
-
     rm(raster_i)
 
     file.remove( file.path(temp_directory, raster_list$file_name[i]) )
-    file.remove( file.path(temp_directory, paste("tf_",raster_list$file_name[i],sep = "")) )
+    file.remove( file.path(temp_directory, paste("temp_",raster_list$file_name[i],sep = "")) )
     gc()
 
   } #i loop
