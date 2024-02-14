0f0b21cdf9a8dee5ef7ee0bd0e1fef87ba4eabf0
Brian <bmaitner@gmail.com>
2024-02-13 22:15:57 -0500

    commenting slow code

--- _targets.R
+++ _targets.R
@@ -124,12 +124,12 @@ list(
                                   domain)
     ),
 
-  tar_target(
-    landcover_za_release,
-    get_release_landcover_za(temp_directory = "data/temp/raw_data/landcover_za/",
-                             tag = "raw_static",
-                             domain = domain)
-    ),
+  # tar_target(
+  #   landcover_za_release,
+  #   get_release_landcover_za(temp_directory = "data/temp/raw_data/landcover_za/",
+  #                            tag = "raw_static",
+  #                            domain = domain)
+  #   ),
 
   # tar_target(
   #   precipitation_chelsa_release,
