13524b728fc66e9c10ef37260bc7621b47800176
Brian <bmaitner@gmail.com>
2024-05-09 00:27:33 -0400

    uncommenting

--- _targets.R
+++ _targets.R
@@ -61,24 +61,24 @@ list(
     vegmap_shp, # 2018 National Vegetation Map http://bgis.sanbi.org/SpatialDataset/Detail/1674
     "data/manual_download/VEGMAP2018_AEA_16082019Final/NVM2018_AEA_V22_7_16082019_final.shp",
     format = "file"
+  ),
+
+  tar_target(
+    remnants_shp,
+    "data/manual_download/RLE_2021_Remnants/RLE_Terr_2021_June2021_Remnants_ddw.shp",
+    format = "file"
+  ),
+
+  tar_target(
+    sanbi_fires_shp,
+    st_read("data/manual_download/All_Fires/All_Fires_20_21_gw.shp")
+  ),
+
+
+  tar_target(
+    country,
+    national_boundary()
   )
-  # ,
-#   tar_target(
-#     remnants_shp,
-#     "data/manual_download/RLE_2021_Remnants/RLE_Terr_2021_June2021_Remnants_ddw.shp",
-#     format = "file"
-#   ),
-#
-#   tar_target(
-#     sanbi_fires_shp,
-#     st_read("data/manual_download/All_Fires/All_Fires_20_21_gw.shp")
-#   ),
-#
-#
-#   tar_target(
-#     country,
-#     national_boundary()
-#   )
 # ,
 #
 #   tar_target(
