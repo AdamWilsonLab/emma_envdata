1d4b62ecefed246a36645eb063137421067eea65
Brian <bmaitner@gmail.com>
2024-05-09 13:41:22 -0400

    uncommenting modis stuff

--- _targets.R
+++ _targets.R
@@ -137,8 +137,7 @@ list(
     get_release_precipitation_chelsa(temp_directory = "data/temp/raw_data/precipitation_chelsa/",
                                      tag = "raw_static",
                                      domain = domain)
-    )
-#,
+    ),
 
 #   ## commented out soil_gcfr_release at present due to API/rdryad issues.
 #   ## Emailed dryad folks on 2024/01/04, it seems the API update broke RDryad
@@ -153,48 +152,49 @@ list(
 #   # ),
 #
 # # # # Frequent updates via releases
-#
-#       tar_age(
-#         fire_modis_release,
-#         get_release_fire_modis(temp_directory = "data/temp/raw_data/fire_modis/",
-#                                tag = "raw_fire_modis",
-#                                domain = domain,
-#                                max_layers = 5,
-#                                sleep_time = 5,
-#                                json_token = json_token,
-#                                verbose = FALSE),
-#         #age = as.difftime(7, units = "days")
-#         #age = as.difftime(1, units = "days")
-#         age = as.difftime(0, units = "hours")
-#       ),
-#
-#       tar_age(
-#         kndvi_modis_release,
-#         get_release_kndvi_modis(temp_directory = "data/temp/raw_data/kndvi_modis/",
-#                                tag = "raw_kndvi_modis",
-#                                domain = domain,
-#                                max_layers = 5,
-#                                sleep_time = 5,
-#                                json_token = json_token,
-#                                verbose = TRUE),
-#         age = as.difftime(7, units = "days")
-#         #age = as.difftime(1, units = "days")
-#         #age = as.difftime(0, units = "hours")
-#     ),
-#
-#     tar_age(
-#       ndvi_modis_release,
-#       get_release_ndvi_modis(temp_directory = "data/temp/raw_data/ndvi_modis/",
-#                               tag = "raw_ndvi_modis",
-#                               domain = domain,
-#                               max_layers = 12,
-#                              sleep_time = 5,
-#                              json_token = json_token),
-#       #age = as.difftime(7, units = "days")
-#       #age = as.difftime(1, units = "days")
-#       age = as.difftime(0, units = "hours")
-#     ),
-#
+
+      tar_age(
+        fire_modis_release,
+        get_release_fire_modis(temp_directory = "data/temp/raw_data/fire_modis/",
+                               tag = "raw_fire_modis",
+                               domain = domain,
+                               max_layers = 5,
+                               sleep_time = 5,
+                               json_token = json_token,
+                               verbose = FALSE),
+        #age = as.difftime(7, units = "days")
+        #age = as.difftime(1, units = "days")
+        age = as.difftime(0, units = "hours")
+      ),
+
+      tar_age(
+        kndvi_modis_release,
+        get_release_kndvi_modis(temp_directory = "data/temp/raw_data/kndvi_modis/",
+                               tag = "raw_kndvi_modis",
+                               domain = domain,
+                               max_layers = 5,
+                               sleep_time = 5,
+                               json_token = json_token,
+                               verbose = TRUE),
+        age = as.difftime(7, units = "days")
+        #age = as.difftime(1, units = "days")
+        #age = as.difftime(0, units = "hours")
+    ),
+
+    tar_age(
+      ndvi_modis_release,
+      get_release_ndvi_modis(temp_directory = "data/temp/raw_data/ndvi_modis/",
+                              tag = "raw_ndvi_modis",
+                              domain = domain,
+                              max_layers = 12,
+                             sleep_time = 5,
+                             json_token = json_token),
+      #age = as.difftime(7, units = "days")
+      #age = as.difftime(1, units = "days")
+      age = as.difftime(0, units = "hours")
+    )
+#,
+
 #     tar_age(
 #       ndvi_viirs_release,
 #       get_release_ndvi_viirs(temp_directory = "data/temp/raw_data/ndvi_viirs/",
