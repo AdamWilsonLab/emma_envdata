d72cdc64b0890802376e339be917fe75b34e3b2c
Jiyeon Kim <59989607+jiyeon-kim20@users.noreply.github.com>
2025-06-13 14:36:56 -0400

    Update _targets.R

--- _targets.R
+++ _targets.R
@@ -89,18 +89,18 @@ library(reticulate)
     #         )
     # googledrive::drive_auth(token = token)
     # ee_Authenticate(auth_mode='gcloud')
-    # ee_Initialize(
-    #   #user = "emma-envdata@ee-wilsonlab-emma.iam.gserviceaccount.com",
-    #   credentials     = "secrets/ee-wilsonlab-emma-ef416058504a.json",
-    #   # credentials = "/github/home/.config/earthengine/service-account-key.json",
-    #   # drive           = TRUE,
-    #   # gcs             = FALSE,
-    #   project           = "ee-wilsonlab-emma",
-    #   auth_mode       = 'appdefault',
-    #   auth_quiet      = TRUE,
-    #   quiet           = TRUE
-    # )
-    ee_Initialize(auth_quiet = TRUE)
+    ee_Initialize(
+      user = "emma-envdata@ee-wilsonlab-emma.iam.gserviceaccount.com",
+      # credentials     = "secrets/ee-wilsonlab-emma-ef416058504a.json",
+      # credentials = "/github/home/.config/earthengine/service-account-key.json",
+      # drive           = TRUE,
+      # gcs             = FALSE,
+      project           = "ee-wilsonlab-emma",
+      auth_mode       = 'appdefault',
+      auth_quiet      = TRUE,
+      quiet           = TRUE
+    )
+    #ee_Initialize(auth_quiet = TRUE)
     message("ee_Initialize is completed")
     # unlink("~/.config/earthengine", recursive = TRUE, force = TRUE)
     # unlink("~/.rgee", recursive = TRUE, force = TRUE)
