410fdb7d7f4da74e348a3e716a7afadba3dd3850
Jiyeon Kim <59989607+jiyeon-kim20@users.noreply.github.com>
2025-06-12 22:30:50 -0400

    Update _targets.R

--- _targets.R
+++ _targets.R
@@ -60,13 +60,14 @@ library(reticulate)
     options(gargle_verbosity = "debug")
     #Initializing with service account key
 
-    # service_account <- jsonlite::read_json(json_token)$client_email
-    # credentials <- ee$ServiceAccountCredentials(service_account, json_token)
+
     # unlink("~/.config/earthengine", recursive = TRUE, force = TRUE)
     ee_Authenticate(auth_mode='appdefault', quiet=TRUE)
     message("Authentication is completed")
     # rgee::ee_clean_credentials()
-    ee$Initialize(project="ee-wilsonlab-emma", quiet=TRUE)
+    service_account <- jsonlite::read_json(json_token)$client_email
+    credentials <- ee$ServiceAccountCredentials(service_account, json_token)
+    ee$Initialize(credentials=credentials)
     message("Initialization is completed")
 
     # point to your service-account JSON
