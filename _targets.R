2e6c95d5837d61214d2a9a9a27e631bc7ac888fb
Jiyeon Kim <59989607+jiyeon-kim20@users.noreply.github.com>
2025-06-19 14:21:06 -0400

    Update _targets.R

--- _targets.R
+++ _targets.R
@@ -108,6 +108,7 @@ library(reticulate)
     #ee_install_upgrade() 
     #ee_Authenticate(auth_mode='appdefault', quiet=TRUE)
     
+    ee_Authenticate()
     ee_Initialize() #auth_mode="appdefault", quiet = TRUEㅣ, credentials=credentials,  project = "ee-wilsonlab-emma", 
     reticulate::py_last_error()
     message("ee_Initialize is completed")
