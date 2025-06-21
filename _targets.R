bb36062e28080c45076038586f7e9ec71cb8f6d3
Jiyeon Kim <59989607+jiyeon-kim20@users.noreply.github.com>
2025-06-21 15:53:19 -0400

    Update _targets.R

--- _targets.R
+++ _targets.R
@@ -108,13 +108,13 @@ library(reticulate)
     # ee_Authenticate(auth_mode='appdefault', quiet=TRUE)
     
     #ee_Authenticate()
-    ee_Initialize(
-                    project   = "ee-wilsonlab-emma",
-                    scopes='https://www.googleapis.com/auth/devstorage.full_control',
-                    credentials=credentials,
-                    auth_mode = "appdefault",
-                    quiet     = TRUE
-                  ) #auth_mode="appdefault", quiet = TRUEㅣ, credentials=credentials,  project = "ee-wilsonlab-emma", 
+    ee_Initialize()
+                  #   project   = "ee-wilsonlab-emma",
+                  #   scopes='https://www.googleapis.com/auth/devstorage.full_control',
+                  #   credentials=credentials,
+                  #   auth_mode = "appdefault",
+                  #   quiet     = TRUE
+                  # ) #auth_mode="appdefault", quiet = TRUEㅣ, credentials=credentials,  project = "ee-wilsonlab-emma", 
     reticulate::py_last_error()
     message("ee_Initialize is completed")
     # unlink("~/.config/earthengine", recursive = TRUE, force = TRUE)
